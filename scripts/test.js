6fdc4ca97fea6c42a50dcd7418dda51c73cd1b9e
Maksym Vorobiov <maxim.vorobjov@gmail.com>
2016-12-05 02:22:25 +0200

    fixed some tests and startup script

--- scripts/test.js
+++ scripts/test.js
@@ -1,12 +1,6 @@
 process.env.NODE_ENV = 'test';
 process.env.PUBLIC_URL = '';
 
-// Load environment variables from .env file. Suppress warnings using silent
-// if this file is missing. dotenv will never modify any environment variables
-// that have already been set.
-// https://github.com/motdotla/dotenv
-require('dotenv').config({silent: true});
-
 const jest = require('jest');
 const argv = process.argv.slice(2);
 
