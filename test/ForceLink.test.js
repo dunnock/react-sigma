0470ed5a9a5a9cae91051c21a21ef5d582f7323a
Maksym Vorobiov <maxim.vorobjov@gmail.com>
2016-12-27 20:53:26 +0200

    fixed ForceLink test

--- test/ForceLink.test.js
+++ test/ForceLink.test.js
@@ -1,6 +1,7 @@
 // @flow
 
-import {Sigma, ForceLink} from '../src/index';
+import {Sigma} from '../src/index';
+import ForceLink from '../src/ForceLink';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { test_graph } from './data'
@@ -10,4 +11,3 @@ it('Sigma with ForceAtlas2 renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Sigma renderer="canvas" graph={test_graph}><ForceLink timeout={10}/></Sigma>, div);
 });
-
