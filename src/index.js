ee754b6e48aaac4b8d64ff8f33731997af278f86
Maksym Vorobiov <maxim.vorobjov@gmail.com>
2016-12-27 20:42:46 +0200

    ForceLink should be imported explicitly

--- src/index.js
+++ src/index.js
@@ -5,15 +5,12 @@ export LoadJSON from './LoadJSON'
 export LoadGEXF from './LoadGEXF'
 export Filter from './Filter'
 export ForceAtlas2 from './ForceAtlas2'
-export ForceLink from './ForceLink'
 export RelativeSize from './RelativeSize'
 export NOverlap from './NOverlap'
 export NeoCypher from './NeoCypher'
 export NeoGraphItemsProducers from './NeoGraphItemsProducers'
 export RandomizeNodePositions from './RandomizeNodePositions'
 export SigmaEnableWebGL from './SigmaEnableWebGL'
-
-//Dagre is not included intentionally in the default distribution, it's huge and should be imported explicitly
 
 import Sigma from './Sigma'
 export default Sigma
