75c8f9a19859bfd48803f4fc6523aeb7376f5c43
Maksym Vorobiov <maxim.vorobjov@gmail.com>
2019-03-13 23:54:53 +0200

    fix tests

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,13 @@
-{
+// babel.config.js
+module.exports = {
   "presets": [
     "@babel/preset-env",
     "@babel/preset-react"
   ],
   "plugins": [
     "flow-react-proptypes",
+    "@babel/plugin-transform-flow-strip-types",
     "@babel/plugin-proposal-export-default-from",
     "@babel/plugin-proposal-class-properties"
   ]
-}
+};
